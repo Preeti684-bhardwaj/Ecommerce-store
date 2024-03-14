612767fe361f115fcda5648e7a2ddd16d5fc677b
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2024-03-14 18:49:23 +0530

    minor changes"

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,10 +1,9 @@
 import path from 'path'
 import { payloadCloud } from '@payloadcms/plugin-cloud'
 import { postgresAdapter } from '@payloadcms/db-postgres'
-import { webpackBundler } from '@payloadcms/bundler-webpack'
 import { slateEditor } from '@payloadcms/richtext-slate'
 import { buildConfig } from 'payload/config'
-
+import { webpackBundler } from '@payloadcms/bundler-webpack'
 import Users from './collections/Users'
 import StoreAttributes from './collections/Store'
 import InventoryAttributes from './collections/Inventory'
